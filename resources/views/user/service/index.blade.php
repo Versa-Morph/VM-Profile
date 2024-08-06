5cc118dafdb1c9b947c61454fb5d03eb9ae3bceb
Sahri Ramadan <sahriramadan000@gmail.com>
2024-08-06 13:10:34 +0700

    Fix: Update Service Page

--- resources/views/user/service/index.blade.php
+++ resources/views/user/service/index.blade.php
@@ -41,10 +41,11 @@
                                     <h4 class="mil-muted mil-up mil-mb-30">Digital <br>Transformation</h4>
                                     <p class="mil-descr mil-light-soft mil-up mil-mb-30">Leading your business through seamless digital transitions for greater efficiency.</p>
                                     <ul class="mil-service-list mil-light mil-mb-30">
-                                        <li class="mil-up">UX Audits</li>
-                                        <li class="mil-up">Design thinking</li>
-                                        <li class="mil-up">Methodologies</li>
-                                        <li class="mil-up">Wireframing</li>
+                                        <li class="mil-up">Assessment & Strategy</li>
+                                        <li class="mil-up">Planning & Design</li>
+                                        <li class="mil-up">Implementation</li>
+                                        <li class="mil-up">Optimization</li>
+                                        <li class="mil-up">Support & Maintenance</li>
                                     </ul>
                                     <a href="{{ route('user.service.detail', ['name' => 'Digital-Transformation']) }}" class="mil-link mil-accent mil-arrow-place mil-up">
                                         <span>Read more</span>
@@ -56,10 +57,12 @@
                                     <h4 class="mil-muted mil-up mil-mb-30">Software <br>Development</h4>
                                     <p class="mil-descr mil-light-soft mil-up mil-mb-30">Designing and developing software tailored to your specific needs.</p>
                                     <ul class="mil-service-list mil-light mil-mb-30">
-                                        <li class="mil-up">UX Audits</li>
-                                        <li class="mil-up">Design thinking</li>
-                                        <li class="mil-up">Methodologies</li>
-                                        <li class="mil-up">Wireframing</li>
+                                        <li class="mil-up">Requirement Analysis</li>
+                                        <li class="mil-up">Design</li>
+                                        <li class="mil-up">Development</li>
+                                        <li class="mil-up">Testing</li>
+                                        <li class="mil-up">Deployment</li>
+                                        <li class="mil-up">Maintenance & Support</li>
                                     </ul>
                                     <a href="{{ route('user.service.detail', ['name' => 'Software-Development']) }}" class="mil-link mil-accent mil-arrow-place mil-up">
                                         <span>Read more</span>
@@ -71,10 +74,10 @@
                                     <h4 class="mil-muted mil-up mil-mb-30">IT <br>Consulting</h4>
                                     <p class="mil-descr mil-light-soft mil-up mil-mb-30">Offering expert guidance to optimize your technology infrastructure.</p>
                                     <ul class="mil-service-list mil-light mil-mb-30">
-                                        <li class="mil-up">UX Audits</li>
-                                        <li class="mil-up">Design thinking</li>
-                                        <li class="mil-up">Methodologies</li>
-                                        <li class="mil-up">Wireframing</li>
+                                        <li class="mil-up">Analysis & Recommendations</li>
+                                        <li class="mil-up">Implementation Planning</li>
+                                        <li class="mil-up">Execution</li>
+                                        <li class="mil-up">Review & Optimization</li>
                                     </ul>
                                     <a href="{{ route('user.service.detail', ['name' => 'IT-Consulting']) }}" class="mil-link mil-accent mil-arrow-place mil-up">
                                         <span>Read more</span>
@@ -86,10 +89,11 @@
                                     <h4 class="mil-muted mil-up mil-mb-30">Data Analytics <br>and AI Solutions</h4>
                                     <p class="mil-descr mil-light-soft mil-up mil-mb-30">Utilizing data and AI to drive strategic business decisions.</p>
                                     <ul class="mil-service-list mil-light mil-mb-30">
-                                        <li class="mil-up">UX Audits</li>
-                                        <li class="mil-up">Design thinking</li>
-                                        <li class="mil-up">Methodologies</li>
-                                        <li class="mil-up">Wireframing</li>
+                                        <li class="mil-up">Data Collection & Preparation</li>
+                                        <li class="mil-up">Analysis & Insights</li>
+                                        <li class="mil-up">AI Model Development</li>
+                                        <li class="mil-up">Implementation</li>
+                                        <li class="mil-up">Monitoring & Optimization</li>
                                     </ul>
                                     <a href="{{ route('user.service.detail', ['name' => 'Data-Analytics-Transformation-and-AI-Solutions']) }}" class="mil-link mil-accent mil-arrow-place mil-up">
                                         <span>Read more</span>
@@ -101,10 +105,11 @@
                                     <h4 class="mil-muted mil-up mil-mb-30">Cybersecurity <br>Services</h4>
                                     <p class="mil-descr mil-light-soft mil-up mil-mb-30">Ensuring robust protection for your digital assets and data.</p>
                                     <ul class="mil-service-list mil-light mil-mb-30">
-                                        <li class="mil-up">UX Audits</li>
-                                        <li class="mil-up">Design thinking</li>
-                                        <li class="mil-up">Methodologies</li>
-                                        <li class="mil-up">Wireframing</li>
+                                        <li class="mil-up">Assessment & Risk Analysis</li>
+                                        <li class="mil-up">Strategy & Planning</li>
+                                        <li class="mil-up">Implementation</li>
+                                        <li class="mil-up">Monitoring & Response</li>
+                                        <li class="mil-up">Maintenance & Updates</li>
                                     </ul>
                                     <a href="{{ route('user.service.detail', ['name' => 'Cybersecurity-Services']) }}" class="mil-link mil-accent mil-arrow-place mil-up">
                                         <span>Read more</span>
@@ -116,10 +121,11 @@
                                     <h4 class="mil-muted mil-up mil-mb-30">Cloud <br>Solutions</h4>
                                     <p class="mil-descr mil-light-soft mil-up mil-mb-30">Implementing scalable cloud services to support your business operations.</p>
                                     <ul class="mil-service-list mil-light mil-mb-30">
-                                        <li class="mil-up">UX Audits</li>
-                                        <li class="mil-up">Design thinking</li>
-                                        <li class="mil-up">Methodologies</li>
-                                        <li class="mil-up">Wireframing</li>
+                                        <li class="mil-up">Assessment & Planning</li>
+                                        <li class="mil-up">Design & Architecture</li>
+                                        <li class="mil-up">Migration</li>
+                                        <li class="mil-up">Deployment</li>
+                                        <li class="mil-up">Management & Optimization</li>
                                     </ul>
                                     <a href="{{ route('user.service.detail', ['name' => 'Cloud-Solutions']) }}" class="mil-link mil-accent mil-arrow-place mil-up">
                                         <span>Read more</span>
